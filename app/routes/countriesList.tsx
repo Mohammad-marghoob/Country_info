f66de740da2c5d510613f7a693c827f08cd37db9
Mohammad-marghoob <marghoob.mohamad1998@gmail.com>
2025-03-25 10:51:32 +0330

    Fetched countries

--- app/routes/countriesList.tsx
+++ app/routes/countriesList.tsx
@@ -1,3 +1,35 @@
-export default function CountriesList() {
-  return <div>list page</div>;
+import { Link } from "react-router";
+import type { Route } from "./+types/countriesList";
+
+interface Country {
+  name: {
+    common: string;
+  };
+  population: number;
+  region: string;
+}
+
+export async function clientLoader() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  return data;
+}
+
+export default function CountriesList({ loaderData }: Route.ComponentProps) {
+  if (!loaderData) return;
+
+  return (
+    <ul>
+      {loaderData.map((country: Country, key: number) => (
+        <>
+          <Link key={key} to={`/countries/${country.name.common}`}>
+            <li>{country.name.common}</li>
+          </Link>
+          <div>
+            Region: {country.region} | Population: {country.population}
+          </div>
+        </>
+      ))}
+    </ul>
+  );
 }
